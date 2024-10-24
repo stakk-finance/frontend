9b6d997336ae06e73a602dfbbb1ae93e5c0a0576
almadendj <almadendj@gmail.com>
2024-10-24 20:20:48 +0800

    add sample hook for template code

---
+++ src/hooks/useSample.tsx
@@ -0,0 +1,25 @@
+// From DJ: This is a sample hook template so that we can be consistent
+
+import React, { createContext, useContext } from "react";
+
+const SampleContext = createContext(null);
+
+export function useSample() {
+  const context = useContext(SampleContext);
+
+  if (!context) throw new Error("useSample must be used within a SampleProvider");
+
+  return context;
+}
+
+
+/*
+ * We put this inside the Providers.tsx component, which is located inside src/app
+ */
+export function SampleProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <SampleContext.Provider value={null}>
+      {children}
+    </SampleContext.Provider>
+  )
+}

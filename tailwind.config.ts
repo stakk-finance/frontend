5c69c6d440644c41203082c8e8043b5a61395904
almadendj <almadendj@gmail.com>
2024-10-24 17:52:25 +0800

    montserrat setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)'
+      },
+      fontFamily: {
+        sans: ['var(--font-montserrat)', 'sans-serif']
       }
     }
   },

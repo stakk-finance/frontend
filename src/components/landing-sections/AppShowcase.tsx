a2b9534541a249dbaed74ecda5a0a55416db9c8f
almadendj <almadendj@gmail.com>
2024-11-02 05:00:18 +0800

    feat: add app preview section

--- src/components/landing-sections/AppShowcase.tsx
+++ src/components/landing-sections/AppShowcase.tsx
@@ -1,7 +1,9 @@
 export default function AppShowcaseSection() {
   return (
-    <div className="min-h-screen">
-      hello
+    <div className="min-h-screen flex flex-col items-center justify-start -mt-12 z-10">
+      <div className="w-lg rounded-xl border border-border h-mainSection backdrop-blur bg-white/[0.08] flex items-center justify-center">
+        This is where we put samples of our app
+      </div>
     </div>
   );
 }

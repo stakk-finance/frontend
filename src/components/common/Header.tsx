99b60caf9e3a28f750311db57377131609ac8565
almadendj <almadendj@gmail.com>
2024-11-02 02:47:02 +0800

    feat: added hover effects to magic button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -13,9 +13,10 @@ export default function Header() {
         className={`transition-transform duration-300 ${isVisible ? "translate-y-0" : "-translate-y-[100px]"} flex flex-row justify-between items-center min-h-header w-lg border-border-light border pl-4 p-2 fixed top-4 rounded-2xl backdrop-blur-md bg-white/[0.08]`}
       >
         <StakkLogoWide />
-        <div className="relative">
-          <div className="absolute -inset-0.5 rounded-lg blur opacity-50 animate-gradient bg-gradient-to-r from-[#e96443] via-primary to-[#e96443]" />
-          <Button className="relative bg-background">Launch App</Button>
+        <div className="group relative inline-block">
+          <div className="absolute -inset-0.5 rounded-lg blur opacity-50 animate-gradient bg-gradient-to-r from-[#e96443] via-primary to-[#e96443] 
+              group-hover:-inset-1 group-hover:blur-lg transition duration-300 ease-in-out" />
+          <Button variant={"secondary"} className="relative bg-background">Launch App</Button>
         </div>
       </div>
     </div>

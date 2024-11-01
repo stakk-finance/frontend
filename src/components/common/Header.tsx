bb4bda045c4f4cbaa2b773961fc4fc2e373a888f
almadendj <almadendj@gmail.com>
2024-11-02 02:49:19 +0800

    chore: moved magic button to its own component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,6 +3,7 @@
 import { Button } from "../ui/button";
 import StakkLogoWide from "./LogoWide";
 import { useHeader } from "@/hooks/useHeader";
+import MagicButton from "./MagicButton";
 
 export default function Header() {
   const { isVisible } = useHeader();
@@ -13,11 +14,7 @@ export default function Header() {
         className={`transition-transform duration-300 ${isVisible ? "translate-y-0" : "-translate-y-[100px]"} flex flex-row justify-between items-center min-h-header w-lg border-border-light border pl-4 p-2 fixed top-4 rounded-2xl backdrop-blur-md bg-white/[0.08]`}
       >
         <StakkLogoWide />
-        <div className="group relative inline-block">
-          <div className="absolute -inset-0.5 rounded-lg blur opacity-50 animate-gradient bg-gradient-to-r from-[#e96443] via-primary to-[#e96443] 
-              group-hover:-inset-1 group-hover:blur-lg transition duration-300 ease-in-out" />
-          <Button variant={"secondary"} className="relative bg-background">Launch App</Button>
-        </div>
+        <MagicButton />
       </div>
     </div>
   );
---
+++ src/components/common/MagicButton.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../ui/button";
+
+export default function MagicButton() {
+  return (
+    <div className="group relative inline-block">
+      <div className="absolute -inset-0.5 rounded-lg blur opacity-50 animate-gradient bg-gradient-to-r from-[#e96443] via-primary to-[#e96443] 
+              group-hover:-inset-1 group-hover:blur-lg transition duration-300 ease-in-out" />
+      <Button variant={"secondary"} className="relative bg-background">Launch App</Button>
+    </div>
+  );
+}

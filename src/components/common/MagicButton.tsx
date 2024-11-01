2b8fdf638cb0ac2ffc681555875f98f56d113e2d
almadendj <almadendj@gmail.com>
2024-11-02 02:56:23 +0800

    fix: minor adjustments to magic button

--- src/components/common/MagicButton.tsx
+++ src/components/common/MagicButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 export default function MagicButton() {
   return (
     <div className="group relative inline-block">
-      <div className="absolute -inset-0.5 rounded-lg blur opacity-50 animate-gradient bg-gradient-to-r from-[#e96443] via-primary to-[#e96443] 
+      <div className="absolute -inset-0.5 rounded-xl blur-sm opacity-50 animate-gradient bg-gradient-to-r from-[#e96443] via-primary to-[#e96443] 
               group-hover:-inset-1 group-hover:blur-lg transition duration-300 ease-in-out" />
       <Button variant={"secondary"} className="relative bg-background">Launch App</Button>
     </div>

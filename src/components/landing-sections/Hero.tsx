ced905c903d3363c2e4a47cccd02dd758210fa5f
almadendj <almadendj@gmail.com>
2024-11-02 13:16:11 +0800

    feat: hero section responsive

--- src/components/landing-sections/Hero.tsx
+++ src/components/landing-sections/Hero.tsx
@@ -9,10 +9,10 @@ export default function HeroSection() {
     <div className="w-full flex justify-center items-center relative overflow-hidden">
       <GridBackground />
       <div className="flex-1 min-h-mainSection max-w-xl flex flex-col justify-center items-center relative text-center">
-        <span className="text-6xl font-bold max-w-lg">
+        <span className="text-4xl md:text-4xl lg:text-6xl font-bold max-w-lg px-6">
           Maximize Your Yield with AI-Powered Staking
         </span>
-        <span className="text-secondary max-w-sm mt-4">
+        <span className="text-secondary max-w-sm px-6 lg:px-0 mt-4 text-xs md:text-sm lg:text-base">
           Benefit from intelligent risk management, airdrop farming, and participate in key governance of leading DeFi projects like JUP and BONK, all in one platform.
         </span>
 

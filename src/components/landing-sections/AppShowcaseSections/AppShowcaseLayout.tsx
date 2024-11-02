2bde38c7b71541f8e998e32616a2290a04703e5b
almadendj <almadendj@gmail.com>
2024-11-03 03:18:00 +0800

    feat: add first card to app showcase

--- src/components/landing-sections/AppShowcaseSections/AppShowcaseLayout.tsx
+++ src/components/landing-sections/AppShowcaseSections/AppShowcaseLayout.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import AppShowcaseSidebar from "./AppShowcaseSidebar"
 
 export default function AppShowcaseLayout() {
@@ -6,7 +7,16 @@ export default function AppShowcaseLayout() {
       <AppShowcaseSidebar />
 
       <div className="flex-1 bg-transparent h-full flex justify-center items-center">
-        ONGOING: The dashboard panel is currently being cooked... (dev cooking)
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Total Staked</CardTitle>
+            {/* <Coins className="h-4 w-4 text-muted-foreground" /> */}
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">1,000 SOL</div>
+            <p className="text-xs text-muted-foreground">+20% from last month</p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );

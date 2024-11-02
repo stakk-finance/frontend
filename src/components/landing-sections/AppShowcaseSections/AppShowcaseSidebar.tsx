5890ec46dc791d748791b79fc711f89bb2b711ae
almadendj <almadendj@gmail.com>
2024-11-03 03:14:16 +0800

    feat: add stakk logo to app showcase

--- src/components/landing-sections/AppShowcaseSections/AppShowcaseSidebar.tsx
+++ src/components/landing-sections/AppShowcaseSections/AppShowcaseSidebar.tsx
@@ -1,10 +1,14 @@
+import StakkLogo from "@/components/common/Logo";
 import { Avatar } from "@/components/ui/avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export default function AppShowcaseSidebar() {
   return (
     <div className="h-full w-[50px] border-r border-border bg-background flex flex-col items-center justify-between">
-      <div className="">
+      <div className="pt-3 flex flex-col gap-3">
+        <div className="">
+          <StakkLogo />
+        </div>
       </div>
       <div className="h-12 w-full flex justify-center items-center p-2">
         <div className="scale-[0.8]">

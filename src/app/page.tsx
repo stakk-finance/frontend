25e7bf73eed7a9a5a1bdf72e0d20ed20023746bf
almadendj <almadendj@gmail.com>
2024-11-08 18:13:57 +0800

    feat: update background to noise + blur bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@
 import AppShowcaseSection from '@/components/landing-sections/AppShowcase';
 import HeroSection from '@/components/landing-sections/Hero';
 import React from 'react';
+import Image from 'next/image';
+import bgHero from "../../public/backgrounds/bg-hero-home.png";
 
 const Home = () => {
   return (
-    <main className='flex flex-col justify-center items-center'>
-      <div className='radial-bg'>
-        <HeroSection />
-        <AppShowcaseSection />
-      </div>
+    <main className='flex flex-col justify-center items-center bg-noise'>
+      <Image
+        className={"z-0 fixed opacity-50 h-full w-full top-0"}
+        src={bgHero}
+        alt={"bg-blur"}
+        priority
+      />
+      <HeroSection />
+      <AppShowcaseSection />
     </main>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,15 +10,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        float: "float 2s ease-in-out infinite",
+        "float-slower": "float 3s ease-in-out infinite",
+        "float-slowest": "float 2.5s ease-in-out infinite",
+        'gradient': 'moveGradient 4s ease infinite'
+      },
+      keyframes: {
+        float: {
+          "0%": { transform: "translateY(0) translateX(0)" },
+          "50%": { transform: "translateY(-5px) translateX(-5px)" },
+          "100%": { transform: "translateY(0) translateX(0)" },
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        noise: "url(/backgrounds/noise.png)",
+        noise2: "url(/backgrounds/noise-bg-2.png)",
+      },
       maxWidth: sizes.container,
       width: sizes.container,
       height: sizes.height,
       minHeight: sizes.height,
       textColor: colors.text,
       colors: colors,
-      animation: {
-        'gradient': 'moveGradient 4s ease infinite'
-      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',

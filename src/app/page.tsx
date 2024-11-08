efca5a2db10c0a30a8b4ad26be75b938508dfa81
almadendj <almadendj@gmail.com>
2024-11-08 18:35:05 +0800

    feat: hero section fade out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,22 @@
 
 import AppShowcaseSection from '@/components/landing-sections/AppShowcase';
 import HeroSection from '@/components/landing-sections/Hero';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import bgHero from "../../public/backgrounds/bg-hero-home.png";
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const Home = () => {
+  const [windowHeight, setWindowHeight] = useState(0);
+  const { scrollY } = useScroll();
+
+  const opacity = useTransform(scrollY, [0, (windowHeight - 200)], [1, 0]);
+  const scale = useTransform(scrollY, [0, windowHeight], [1, 0.8]);
+
+  useEffect(() => {
+    setWindowHeight(window.innerHeight);
+  }, []);
+
   return (
     <main className='flex flex-col justify-center items-center bg-noise'>
       <div className='z-0 fixed opacity-50 radial-bg top-0 w-full h-full'> </div>
@@ -16,8 +27,20 @@ const Home = () => {
         alt={"bg-blur"}
         priority
       />
-      <HeroSection />
-      <AppShowcaseSection />
+      <motion.div
+        style={{
+          opacity,
+          scale,
+          transformOrigin: 'center center'
+        }}
+        className="fixed top-0 left-0 w-full h-full z-10"
+      >
+        <HeroSection />
+      </motion.div>
+      <div className="relative z-20 w-full">
+        <div style={{ height: '100vh' }} /> {/* Spacer */}
+        <AppShowcaseSection />
+      </div>
     </main>
   );
 };
